112e728c5e0d7b746a2d53fd013377fcc06b5db7
riddarvid <riddarvid@gmail.com>
2020-05-03 02:13:09 +0200

    Add cool styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,3 @@
-function repeat(string, times) {
-    let output = "";
-    for (let i = 0; i < times; i++) {
-        output += string;
-        output += " ";
-    }
-    return output;
-}
-
-const yeetParagraph = document.querySelector('p');
-//yeetParagraph.textContent = repeat("swag", 5);
+const heading = document.querySelector('h1');
+let newTitle = "Tjenare mannen"
+//heading.textContent = newTitle;
